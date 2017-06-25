0b212e78fea5610d16a6aed5e14fc19894b84254
mikiohandy <miki1511@gmail.com>
2017-06-25 14:58:09 +0200

    swithcbuttons

--- js/myapp.js
+++ js/myapp.js
@@ -67,8 +67,8 @@ function display (){
                 var output = '<table class="table table-hover"><thead><tr><th>#</th><th>Ime</th><th>Prezime</th><th>Adresa</th>         <th>Telefon</th><th>Članarina</th></tr></thead><tbody>';
                 
                  $.each(data, function(index,data){
-                     
-                     var switchButton = '<div id="proba" class="switch"><label>Off<input data-id="'+data._id.$oid+'" data-switchstatus="'+data.clanarina+'"type="checkbox"><span class="lever"></span>On</label></div>';
+                     var checkStatus = data.clanarina ? 'checked = "checked"' : '',
+                         switchButton = '<div class="switch"><label>Off<input id="proba" data-id="'+data._id.$oid+'" data-switchstatus="'+data.clanarina+'"type="checkbox"'+ checkStatus +'><span class="lever"></span>On</label></div>';
                      
                     output += '<tr><th scope="row">'+ (index + 1) +'</th><td>'+data.ime+'</td><td>'+data.prezime+'</td><td>'+data.adresa+'</td><td>'+data.telefon+'</td><td>'+switchButton+'</td></tr>';
                     });
@@ -80,32 +80,57 @@ function display (){
     
     
 // SWITCH BUTTON
-    
+   
 $('#display-row').on('click', 'input', function(){
         var id = $(this).data('id'),
             switchCheck = $(this).data('switchstatus'),
                // console.log(switchCheck),
             idUrl = 'https://api.mlab.com/api/1/databases/my-first-mongo/collections/members/'+id+'?apiKey=nzIFzNz9TATGyU-0a7h37qlZTP-RpBU4';
             
+    switchCheck = !switchCheck;
+    
     $.ajax({ 
           url: idUrl,
 		  data: JSON.stringify(
-                { "$set" : { "clanarina" : true } }),
+                { "$set" : { "clanarina" : switchCheck } }),
 		  type: "PUT",
 		  contentType: "application/json"
+    }).fail(function(data){
+        alert ('Failed!');
     })
             console.log(switchCheck);
     
     
 });
-              
 
 
+/*
+$('#display-row').on('click','#proba', function(){
     
     
     
+              var checkStatus = $(this).data('switchstatus');
+              var dataId = $(this).data('id');
     
     
+    
+            $.ajax({ 
+                    url: 'https://api.mlab.com/api/1/databases/my-first-mongo/collections/members/'+dataId+'?apiKey=nzIFzNz9TATGyU-0a7h37qlZTP-RpBU4',
+                    type: "GET"
+            }).done(function(data){
+                
+                if(data){
+                    for (var i=0; i<data.length; i++){
+                        if(data[i].id === dataId){
+                            data[i].clanarina = !data[i].clanarina;
+                        }
+                    }
+                }
+                console.log(data.clanarina);
+            })                    
+});
+
+*/
     
     
     
